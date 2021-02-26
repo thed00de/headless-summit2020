1410bbd4f795d4c6d84a637ba88d87a07c487954
Ivan Coughlan <ivan.coughlan@wpengine.com>
2021-02-26 18:10:34 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 module.exports = () => {
+  async headers() {
+    return [
+      {
+        // This works, and returns appropriate Response headers:
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+          },
+        ],
+      },
+      {
+        // This doesn't work for 'Cache-Control' key (works for others though):
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            // Instead of this value:
+            value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180',
+            // Cache-Control response header is `public, max-age=60` in production
+            // and `public, max-age=0, must-revalidate` in development
+          },
+        ],
+      },
+    ]
+  },
   return {
     env: {
       CATEGORIES:
